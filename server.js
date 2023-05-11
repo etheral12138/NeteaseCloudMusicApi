2d934fa7fba41a18d93459e72318db2e06bd5491
etheral12138 <1694815257@qq.com>
2023-05-11 11:01:10 +0800

    初始化

--- server.js
+++ server.js
@@ -158,7 +158,7 @@ async function consturctServer(moduleDefs) {
     if (req.path !== '/' && !req.path.includes('.')) {
       res.set({
         'Access-Control-Allow-Origin':
-          req.headers.origin || 'https://netease.etheral.cc',
+          req.headers.origin || 'https://netease.etheral.cc' || '*',
         'Access-Control-Allow-Credentials': true,
         'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type',
         'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
